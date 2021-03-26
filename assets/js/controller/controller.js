969ac41a163239bf42751d23d48412d34c137abd
Sofiane <sa.dev672@gmail.com>
2021-03-26 22:38:44 +0100

    Amélioration de l'ajout au panier - ajout de la quantité

--- assets/js/controller/controller.js
+++ assets/js/controller/controller.js
@@ -43,33 +43,53 @@ class Controller{
 
             // Création de l'objet correspondant a un produit qui sera stocké dans le local storage
             const newCart = {
-                _id: product._id, 
-                name: product.name, 
+                _id: product._id,
+                name: product.name,
                 imageUrl: product.imageUrl,
-                price: product.price, 
-                lense: selectElement.value
+                price: product.price,
+                lense: selectElement.value,
+                quantity: 1
             };
-            
-            this.cart.push(newCart);        
+
+
+            this.cart.push(newCart);
 
             let dataToBeStored;
 
             if(localStorage.getItem("cameras")){
                 const cameras = JSON.parse(localStorage.getItem("cameras"));
-                cameras.push(newCart)
+
+                console.log(cameras)
+                
+                //Si le produit ajouté au panier existe déja dans le localStorage, on incrémente la quantité de 1
+                if (findOneProduct(cameras, product._id)){
+
+                    const newProduct = findOneProduct(cameras, product._id);
+                    newProduct.quantity ++;
+
+                    const index = cameras.indexOf(findOneProduct(cameras, product._id));
+
+                    cameras.splice(index,1);   
+                    cameras.push(newProduct);
+
+                }else{
+                    cameras.push(newCart);
+                }
+
+                
                 dataToBeStored = cameras;
             }else{
                 dataToBeStored = this.cart;
             }
 
-             //Stock les produits dans le local storage
+            //Stock les produits dans le local storage
             localStorage.setItem("cameras", JSON.stringify(dataToBeStored));
         });
 
-        const alertSuccess = document.querySelector(".alert--green");
-        alertSuccess.classList.remove("alert--hidden");
+        // const alertSuccess = document.querySelector(".alert--green");
+        // alertSuccess.classList.remove("alert--hidden");
 
-        setTimeout(() => alertSuccess.classList.add("alert--hidden"), 2000);
+        // setTimeout(() => alertSuccess.classList.add("alert--hidden"), 2000);
     }
 
     /**
@@ -79,7 +99,7 @@ class Controller{
         const id = e.target.dataset.id;
 
         const productStored = JSON.parse(localStorage.getItem("cameras"));
-        
+
         const productToBeRemoved = findOneProduct(productStored, id);
         
         const index = productStored.indexOf(productToBeRemoved);
